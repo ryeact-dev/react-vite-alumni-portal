4446e8f0affeda46bc02a9653aaed3c4247a9c5c
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-13 20:19:12 +0800

    Remove server folder

--- server/utils/error.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(statusCode, message) {
-  const error = new Error();
-  error.statusCode = statusCode;
-  error.message = message;
-  return error;
-}
-
-exports.errorHandler = errorHandler;
