cfcbd5a0fc52cec7aab98a62f17ef4eb89d3a1bc
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-13 17:47:16 +0800

    complete footer

--- client/src/common/Footer/index.jsx
+++ client/src/common/Footer/index.jsx
@@ -11,88 +11,52 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer className='w-full text-slate-400 bg-base-100'>
-      <article className=' py-4  max-w-6xl mx-auto'>
-        <div className='container px-4 mx-auto'>
-          <div className='-mx-4 flex flex-wrap justify-between'>
-            <div className='px-4 my-4 w-full xl:w-1/5'>
-              <h2 className='text-2xl font-semibold mb-6'>Alumni Portal</h2>
-              <p className='text-justify'>
-                Tailwindow is a collection of UI Components created using
-                Tailwind CSS Framework. The UI Components gives you all of the
-                building blocks you need to build any designs without any
-                annoying opinionated styles you have to fight to override.
-              </p>
-            </div>
+      <article className=' py-4 max-w-6xl mx-auto'>
+        <div className='grid grid-cols-3 my-8'>
+          <div className='px-4 w-ful'>
+            <h2 className='text-2xl font-semibold mb-8'>Alumni Portal</h2>
+            <p className='text-justify'>
+              is a website or app that connects former students of a school,
+              college, or university. It can help you build a strong network,
+              engage your alumni, and showcase your achievements.
+            </p>
+          </div>
 
-            <div className='px-4 my-4 w-full sm:w-auto'>
-              <div>
-                <h2 className='inline-block text-2xl pb-4 mb-4 border-b-4 border-secondary'>
-                  Company
-                </h2>
-              </div>
-              <ul className='leading-8'>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    About Us
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    Terms &amp; Conditions
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    Contact Us
-                  </Link>
-                </li>
-              </ul>
+          <div className='px-4 w-full'>
+            <h2 className='inline-block text-2xl pb-4 mb-2 border-b-4 border-secondary w-full'>
+              Company
+            </h2>
+            <ul className='leading-8'>
+              <li>
+                <Link to='/' className='hover:text-secondary'>
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to='/' className='hover:text-secondary'>
+                  Terms &amp; Conditions
+                </Link>
+              </li>
+              <li>
+                <Link to='/' className='hover:text-secondary'>
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link to='/' className='hover:text-secondary'>
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className='px-4  w-full'>
+            <div>
+              <h2 className='inline-block text-2xl pb-4 mb-4 border-b-4 border-secondary w-full'>
+                Connect With Us
+              </h2>
             </div>
-            <div className='px-4 my-4 w-full sm:w-auto'>
-              <div>
-                <h2 className='inline-block text-2xl pb-4 mb-4 border-b-4 border-secondary'>
-                  Blog
-                </h2>
-              </div>
-              <ul className='leading-8'>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    Getting Started With HTML and CSS
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    What Is Flex And When to Use It?
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    How TailwindCSS Can Help Your Productivity?
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    5 Tips to Make Responsive Website
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/' className='hover:text-secondary'>
-                    See More
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className='px-4 my-4 w-full sm:w-auto xl:w-1/5'>
-              <div>
-                <h2 className='inline-block text-2xl pb-4 mb-4 border-b-4 border-secondary'>
-                  Connect With Us
-                </h2>
-              </div>
+            <div className='flex flex-wrap gap-2'>
               <Link
                 to='/'
                 className='inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-secondary hover:border-blue-400'
